391161201242c054e96ffac6e16edf070ca93035
MTLiew <maxl24usa@gmail.com>
2023-04-04 22:15:56 -0700

    Add project files.

---
+++ GenshinArtifactFarmer/Artifact.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenshinArtifactFarmer
+{
+    public class Artifact
+    {
+        private string name { get; set; }
+
+        private float flatATK { get; set; }
+        private float percentATK { get; set; }
+
+        private float flatHP { get; set; }
+        private float percentHP { get; set; }
+
+        private float flatDEF { get; set; }
+        private float percentDEF { get; set; }
+        
+        private float ER { get; set; }
+        private float EM { get; set; }
+
+        private float critRate { get; set; }
+        private float critDMG { get; set; }
+
+        private bool usesATK { get; set; }
+        private bool usesHP { get; set; }
+        private bool usesDEF { get; set; }
+        private bool usesER { get; set; }
+        private bool usesEM { get; set; }
+        private bool usesCrit { get; set; }
+
+        public Artifact() { }
+
+        public Artifact(float flatATK, float percentATK, float flatHP, float percentHP, float flatDEF, float percentDEF, float ER, float EM, float critRate, float critDMG)
+        {
+            this.flatATK = flatATK;
+            this.percentATK = percentATK;
+
+            this.flatHP = flatHP;
+            this.percentHP = percentHP;
+
+            this.flatDEF = flatDEF;
+            this.percentDEF = percentDEF;
+
+            this.ER = ER;
+            this.EM = EM;
+
+            this.critRate = critRate;
+            this.critDMG = critDMG;
+        }
+
+        public float[] rollValues(float f1, float f2, float f3, float f4)
+        {
+            float[] values = { 0, 0, 0, 0 };
+
+            return values;
+
+        }
+
+        public float substatValue()
+        {
+            float sum = 0;
+
+            if (usesATK)
+            {
+                sum += flatATK / 40;
+                sum += percentATK / 5;
+            }
+
+            if (usesHP)
+            {
+                sum += flatHP / 254;
+                sum += percentHP / 5;
+            }
+
+            if (usesDEF)
+            {
+                sum += flatDEF / 19;
+                sum += percentDEF / (float)6.2;
+            }
+
+            if (usesER) sum += ER / 5;
+            if (usesEM) sum += EM / 20;
+
+            if (usesCrit)
+            {
+                sum += critRate / (float)3.3;
+                sum += critDMG / (float)6.6;
+            }
+
+            return sum;
+        }
+
+        public float rollQuality()
+        {
+            if (flatATK > 0)
+            {
+
+            }
+
+            if (percentATK > 0)
+            {
+
+            }
+
+            if (flatHP > 0)
+            {
+
+            }
+
+            if (percentHP > 0)
+            {
+
+            }
+
+            if (flatDEF > 0)
+            {
+
+            }
+
+            if (percentDEF > 0)
+            {
+
+            }
+
+            if (ER > 0)
+            {
+
+            }
+
+            if (EM > 0)
+            {
+
+            }
+
+            if (critRate > 0)
+            {
+
+            }
+
+            if (critDMG > 0)
+            {
+
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ GenshinArtifactFarmer/GenshinValleyHome.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace GenshinArtifactFarmer
+{
+    /// <summary>
+    /// Interaction logic for GenshinValleyHome.xaml
+    /// </summary>
+    /// 
+
+
+    public partial class GenshinValleyHome : Page
+    {
+        public GenshinValleyHome()
+        {
+            InitializeComponent();
+        }
+
+        private void Register_New_Character_Button(object sender, RoutedEventArgs e)
+        {
+            RegisterNewCharacterPage registerNewCharacterPage = new RegisterNewCharacterPage();
+            this.NavigationService.Navigate(registerNewCharacterPage);
+        }
+
+        private void Import_Artifacts_Button(object sender, RoutedEventArgs e)
+        {
+            string file = "";
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Select a File";
+
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                file = openFileDialog.FileName;
+            }
+
+            using (StreamReader r = new StreamReader(file))
+            {
+                string jsonTemp = r.ReadToEnd();
+                List<Artifact> artifacts = JsonConvert.DeserializeObject<List<Artifact>>(jsonTemp);
+            }
+            
+
+            openFileDialog = null;
+        }
+    }
+}
